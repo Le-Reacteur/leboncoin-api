ff585fecf5b24da08b2c6861ae95ec1a69c7f8cd
Xavier Colombel <pixelgamma@gmail.com>
2018-11-22 16:45:48 +0100

    adding a new route to get offers count

--- routes/offer.js
+++ routes/offer.js
@@ -125,6 +125,67 @@ router.get("/with-counter", function(req, res) {
   });
 });
 
+router.get("/with-count", function(req, res) {
+  const filter = {};
+  if (
+    (req.query.priceMin !== undefined && req.query.priceMin !== "") ||
+    (req.query.priceMax !== undefined && req.query.priceMax !== "")
+  ) {
+    filter.price = {};
+    if (req.query.priceMin) {
+      filter.price["$gte"] = req.query.priceMin;
+    }
+
+    if (req.query.priceMax) {
+      filter.price["$lte"] = req.query.priceMax;
+    }
+  }
+
+  if (req.query.title) {
+    filter.title = {
+      $regex: req.query.title,
+      $options: "i"
+    };
+  }
+
+  Offer.count(filter, (err, count) => {
+    const query = Offer.find(filter).populate({
+      path: "creator",
+      select: "account"
+    });
+
+    if (req.query.skip !== undefined) {
+      query.skip(parseInt(req.query.skip));
+    }
+    if (req.query.limit !== undefined) {
+      query.limit(parseInt(req.query.limit));
+    } else {
+      // valeur par défaut de la limite
+      query.limit(100);
+    }
+
+    switch (req.query.sort) {
+      case "price-desc":
+        query.sort({ price: -1 });
+        break;
+      case "price-asc":
+        query.sort({ price: 1 });
+        break;
+      case "date-desc":
+        query.sort({ created: -1 });
+        break;
+      case "date-asc":
+        query.sort({ created: 1 });
+        break;
+      default:
+    }
+
+    query.exec((err, offers) => {
+      res.json({ count, offers });
+    });
+  });
+});
+
 router.get("/my-offers", isAuthenticated, function(req, res) {
   Offer.find({ creator: req.user })
     .populate({ path: "creator", select: "account" })
