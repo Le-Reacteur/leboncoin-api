78e5ac2d0d3ef81f4e15b6e0a0028daa9a63e099
Etienne Dldc <e.deladonchamps@gmail.com>
2019-08-07 22:42:42 +0200

    Update upload

--- routes/offer.js
+++ routes/offer.js
@@ -1,36 +1,43 @@
-var express = require("express");
+var express = require('express');
 var router = express.Router();
-var Offer = require("../models/Offer.js");
-var isAuthenticated = require("../middlewares/isAuthenticated");
-var uploadPictures = require("../middlewares/uploadPictures");
-var ObjectId = require("mongoose").Types.ObjectId;
+var Offer = require('../models/Offer.js');
+var isAuthenticated = require('../middlewares/isAuthenticated');
+var ObjectId = require('mongoose').Types.ObjectId;
+// Importation de Cloudinary
+var cloudinary = require('cloudinary');
+// Configuration de Cloudinary
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
 
-router.get("/", function(req, res) {
+router.get('/', function(req, res) {
   const filter = {};
   if (
-    (req.query.priceMin !== undefined && req.query.priceMin !== "") ||
-    (req.query.priceMax !== undefined && req.query.priceMax !== "")
+    (req.query.priceMin !== undefined && req.query.priceMin !== '') ||
+    (req.query.priceMax !== undefined && req.query.priceMax !== '')
   ) {
     filter.price = {};
     if (req.query.priceMin) {
-      filter.price["$gte"] = req.query.priceMin;
+      filter.price['$gte'] = req.query.priceMin;
     }
 
     if (req.query.priceMax) {
-      filter.price["$lte"] = req.query.priceMax;
+      filter.price['$lte'] = req.query.priceMax;
     }
   }
 
   if (req.query.title) {
     filter.title = {
       $regex: req.query.title,
-      $options: "i"
+      $options: 'i',
     };
   }
 
   const query = Offer.find(filter).populate({
-    path: "creator",
-    select: "account"
+    path: 'creator',
+    select: 'account',
   });
 
   if (req.query.skip !== undefined) {
@@ -44,16 +51,16 @@ router.get("/", function(req, res) {
   }
 
   switch (req.query.sort) {
-    case "price-desc":
+    case 'price-desc':
       query.sort({ price: -1 });
       break;
-    case "price-asc":
+    case 'price-asc':
       query.sort({ price: 1 });
       break;
-    case "date-desc":
+    case 'date-desc':
       query.sort({ created: -1 });
       break;
-    case "date-asc":
+    case 'date-asc':
       query.sort({ created: 1 });
       break;
     default:
@@ -64,33 +71,33 @@ router.get("/", function(req, res) {
   });
 });
 
-router.get("/with-counter", function(req, res) {
+router.get('/with-counter', function(req, res) {
   const filter = {};
   if (
-    (req.query.priceMin !== undefined && req.query.priceMin !== "") ||
-    (req.query.priceMax !== undefined && req.query.priceMax !== "")
+    (req.query.priceMin !== undefined && req.query.priceMin !== '') ||
+    (req.query.priceMax !== undefined && req.query.priceMax !== '')
   ) {
     filter.price = {};
     if (req.query.priceMin) {
-      filter.price["$gte"] = req.query.priceMin;
+      filter.price['$gte'] = req.query.priceMin;
     }
 
     if (req.query.priceMax) {
-      filter.price["$lte"] = req.query.priceMax;
+      filter.price['$lte'] = req.query.priceMax;
     }
   }
 
   if (req.query.title) {
     filter.title = {
       $regex: req.query.title,
-      $options: "i"
+      $options: 'i',
     };
   }
 
   Offer.count({}, (err, count) => {
     const query = Offer.find(filter).populate({
-      path: "creator",
-      select: "account"
+      path: 'creator',
+      select: 'account',
     });
 
     if (req.query.skip !== undefined) {
@@ -104,16 +111,16 @@ router.get("/with-counter", function(req, res) {
     }
 
     switch (req.query.sort) {
-      case "price-desc":
+      case 'price-desc':
         query.sort({ price: -1 });
         break;
-      case "price-asc":
+      case 'price-asc':
         query.sort({ price: 1 });
         break;
-      case "date-desc":
+      case 'date-desc':
         query.sort({ created: -1 });
         break;
-      case "date-asc":
+      case 'date-asc':
         query.sort({ created: 1 });
         break;
       default:
@@ -125,33 +132,33 @@ router.get("/with-counter", function(req, res) {
   });
 });
 
-router.get("/with-count", function(req, res) {
+router.get('/with-count', function(req, res) {
   const filter = {};
   if (
-    (req.query.priceMin !== undefined && req.query.priceMin !== "") ||
-    (req.query.priceMax !== undefined && req.query.priceMax !== "")
+    (req.query.priceMin !== undefined && req.query.priceMin !== '') ||
+    (req.query.priceMax !== undefined && req.query.priceMax !== '')
   ) {
     filter.price = {};
     if (req.query.priceMin) {
-      filter.price["$gte"] = req.query.priceMin;
+      filter.price['$gte'] = req.query.priceMin;
     }
 
     if (req.query.priceMax) {
-      filter.price["$lte"] = req.query.priceMax;
+      filter.price['$lte'] = req.query.priceMax;
     }
   }
 
   if (req.query.title) {
     filter.title = {
       $regex: req.query.title,
-      $options: "i"
+      $options: 'i',
     };
   }
 
   Offer.count(filter, (err, count) => {
     const query = Offer.find(filter).populate({
-      path: "creator",
-      select: "account"
+      path: 'creator',
+      select: 'account',
     });
 
     if (req.query.skip !== undefined) {
@@ -165,16 +172,16 @@ router.get("/with-count", function(req, res) {
     }
 
     switch (req.query.sort) {
-      case "price-desc":
+      case 'price-desc':
         query.sort({ price: -1 });
         break;
-      case "price-asc":
+      case 'price-asc':
         query.sort({ price: 1 });
         break;
-      case "date-desc":
+      case 'date-desc':
         query.sort({ created: -1 });
         break;
-      case "date-asc":
+      case 'date-asc':
         query.sort({ created: 1 });
         break;
       default:
@@ -186,19 +193,19 @@ router.get("/with-count", function(req, res) {
   });
 });
 
-router.get("/my-offers", isAuthenticated, function(req, res) {
+router.get('/my-offers', isAuthenticated, function(req, res) {
   Offer.find({ creator: req.user })
-    .populate({ path: "creator", select: "account" })
+    .populate({ path: 'creator', select: 'account' })
     .exec(function(err, offers) {
       res.json(offers);
     });
 });
 
-router.delete("/remove/:id", isAuthenticated, function(req, res, next) {
+router.delete('/remove/:id', isAuthenticated, function(req, res, next) {
   Offer.findOneAndRemove(
     {
       _id: ObjectId(req.params.id),
-      creator: req.user
+      creator: req.user,
     },
     function(err, obj) {
       if (err) {
@@ -206,34 +213,60 @@ router.delete("/remove/:id", isAuthenticated, function(req, res, next) {
       }
       if (!obj) {
         res.status(404);
-        return next("Nothing to delete");
+        return next('Nothing to delete');
       } else {
-        return res.json({ message: "Deleted" });
+        return res.json({ message: 'Deleted' });
       }
     }
   );
 });
 
-// router.get("/modifier/:id", function(req, res) {   if (req.isAuthenticated())
-// {     Ad.find({ _id: req.params.id }, function(err, ad) { console.log(ad);
-// if (ad.length) {         res.render("edit", {  infos: getInfos(req,
-// "/modifier"),           ad         });       }     });  } else {
-// res.redirect("/");   } }); router.post("/publier", upload.array("photos",
-// 10), function(req, res) {
-router.post("/publish", isAuthenticated, uploadPictures, function(
-  req,
-  res,
-  next
-) {
-  // var photos = []; if (req.files.length) {   photos = _.map(req.files,
-  // function(file) {     return file.filename;   }); }
+router.post('/upload', isAuthenticated, (req, res) => {
+  // les différents clés des fichiers (file1, file2, file3...)
+  const files = Object.keys(req.files);
+  if (files.length) {
+    const results = {};
+    // on parcours les fichiers
+    files.forEach(fileKey => {
+      // on utilise les path de chaque fichier (la localisation temporaire du fichier sur le serveur)
+      cloudinary.v2.uploader.upload(
+        req.files[fileKey].path,
+        {
+          // on peut préciser un dossier
+          folder: 'some_folder',
+        },
+        (error, result) => {
+          // on enregistre le résultat dans un object
+          if (error) {
+            results[fileKey] = {
+              success: false,
+              error: error,
+            };
+          } else {
+            results[fileKey] = {
+              success: true,
+              result: result,
+            };
+          }
+          if (Object.keys(results).length === files.length) {
+            // tous les uploads sont fait on peut envoyer la réponse
+            return res.json(results);
+          }
+        }
+      );
+    });
+  } else {
+    res.send('no file uploaded');
+  }
+});
 
+router.post('/publish', isAuthenticated, function(req, res, next) {
   var obj = {
     title: req.body.title,
     description: req.body.description,
     price: req.body.price,
     pictures: req.pictures,
-    creator: req.user
+    creator: req.user,
   };
   var offer = new Offer(obj);
   offer.save(function(err) {
@@ -247,8 +280,8 @@ router.post("/publish", isAuthenticated, uploadPictures, function(
         created: offer.created,
         creator: {
           account: offer.creator.account,
-          _id: offer.creator._id
-        }
+          _id: offer.creator._id,
+        },
       });
     } else {
       return next(err.message);
@@ -256,16 +289,16 @@ router.post("/publish", isAuthenticated, uploadPictures, function(
   });
 });
 
-router.get("/:id", function(req, res, next) {
+router.get('/:id', function(req, res, next) {
   Offer.findById(req.params.id)
-    .populate({ path: "creator", select: "account" })
+    .populate({ path: 'creator', select: 'account' })
     .exec(function(err, offer) {
       if (err) {
         return next(err.message);
       }
       if (!offer) {
         res.status(404);
-        return next("Not found");
+        return next('Not found');
       } else {
         return res.json(offer);
       }
--- server.js
+++ server.js
@@ -3,49 +3,55 @@ Le package `dotenv` permet de definir des variables d'environnement
 dans le fichier `.env`. Nous utilisons le fichier `.slugignore` pour ignorer
 le fichier `.env` dans l'environnement Heroku
 */
-require("dotenv").config();
+require('dotenv').config();
 
 /*
 Le package `mongoose` est un ODM (Object-Document Mapping) permettant de
 la manipulation des documents de la base de données comme si s'agissait d'objets
 */
-var mongoose = require("mongoose");
+var mongoose = require('mongoose');
 mongoose.connect(
   process.env.MONGODB_URI,
   {
-    useNewUrlParser: true
+    useNewUrlParser: true,
   },
   function(err) {
-    if (err) console.error("Could not connect to mongodb.");
+    if (err) console.error('Could not connect to mongodb.');
   }
 );
 
-var express = require("express");
+var express = require('express');
 var app = express();
+
+/*
+express-formidable permet de gérer les fichiers envoyer en FormData
+*/
+var formidableMiddleware = require('express-formidable');
+app.use(formidableMiddleware());
 
 /*
 Le package `helmet` est une collection de protections contre certaines
 vulnérabilités HTTP
 */
-var helmet = require("helmet");
+var helmet = require('helmet');
 app.use(helmet());
 
 /*
 Les réponses (> 1024 bytes) du serveur seront compressées au format GZIP pour
 diminuer la quantité d'informations transmises
 */
-var compression = require("compression");
+var compression = require('compression');
 app.use(compression());
 
 // Parse le `body` des requêtes HTTP reçues
-var bodyParser = require("body-parser");
-app.use(bodyParser.json({ limit: "50mb" })); // L'upload est fixée à 50mb maximum (pour l'envoi de fichiers)
+var bodyParser = require('body-parser');
+app.use(bodyParser.json({ limit: '50mb' })); // L'upload est fixée à 50mb maximum (pour l'envoi de fichiers)
 
 // Initialisation des models
-var User = require("./models/User");
+var User = require('./models/User');
 
-app.get("/", function(req, res) {
-  res.send("Welcome to the leboncoin API.");
+app.get('/', function(req, res) {
+  res.send('Welcome to the leboncoin API.');
 });
 
 /*
@@ -53,25 +59,25 @@ app.get("/", function(req, res) {
 requêtes provenant d'un nom de domaine différent. Ici, nous autorisons l'API
 à repondre aux requêtes AJAX venant d'autres serveurs.
 */
-var cors = require("cors");
-app.use("/api", cors());
+var cors = require('cors');
+app.use('/api', cors());
 
 // Les routes sont séparées dans plusieurs fichiers
-var coreRoutes = require("./routes/core.js");
-var userRoutes = require("./routes/user.js");
-var offerRoutes = require("./routes/offer.js");
+var coreRoutes = require('./routes/core.js');
+var userRoutes = require('./routes/user.js');
+var offerRoutes = require('./routes/offer.js');
 
 // Les routes relatives aux utilisateurs auront pour prefix d'URL `/user`
-app.use("/api", coreRoutes);
-app.use("/api/user", userRoutes);
-app.use("/api/offer", offerRoutes);
+app.use('/api', coreRoutes);
+app.use('/api/user', userRoutes);
+app.use('/api/offer', offerRoutes);
 
 /*
 Toutes les méthodes HTTP (GET, POST, etc.) des pages non trouvées afficheront
 une erreur 404
 */
-app.all("*", function(req, res) {
-  res.status(404).json({ error: "Not Found" });
+app.all('*', function(req, res) {
+  res.status(404).json({ error: 'Not Found' });
 });
 
 /*
