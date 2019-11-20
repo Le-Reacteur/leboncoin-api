6a95674019e63d97db51a3469bd39e3c12c7557c
Farid Safi <FaridSafi@users.noreply.github.com>
2019-11-20 08:45:35 +0100

    fix upload

--- routes/offer.js
+++ routes/offer.js
@@ -1,46 +1,47 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-var Offer = require('../models/Offer.js');
-var isAuthenticated = require('../middlewares/isAuthenticated');
-var ObjectId = require('mongoose').Types.ObjectId;
+var Offer = require("../models/Offer.js");
+var isAuthenticated = require("../middlewares/isAuthenticated");
+var ObjectId = require("mongoose").Types.ObjectId;
+const uid2 = require("uid2");
 
-var formidableMiddleware = require('express-formidable');
+var formidableMiddleware = require("express-formidable");
 
 // Importation de Cloudinary
-var cloudinary = require('cloudinary');
+var cloudinary = require("cloudinary");
 // Configuration de Cloudinary
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_API_SECRET,
+  api_secret: process.env.CLOUDINARY_API_SECRET
 });
 
-router.get('/', function(req, res) {
+router.get("/", function(req, res) {
   const filter = {};
   if (
-    (req.query.priceMin !== undefined && req.query.priceMin !== '') ||
-    (req.query.priceMax !== undefined && req.query.priceMax !== '')
+    (req.query.priceMin !== undefined && req.query.priceMin !== "") ||
+    (req.query.priceMax !== undefined && req.query.priceMax !== "")
   ) {
     filter.price = {};
     if (req.query.priceMin) {
-      filter.price['$gte'] = req.query.priceMin;
+      filter.price["$gte"] = req.query.priceMin;
     }
 
     if (req.query.priceMax) {
-      filter.price['$lte'] = req.query.priceMax;
+      filter.price["$lte"] = req.query.priceMax;
     }
   }
 
   if (req.query.title) {
     filter.title = {
       $regex: req.query.title,
-      $options: 'i',
+      $options: "i"
     };
   }
 
   const query = Offer.find(filter).populate({
-    path: 'creator',
-    select: 'account',
+    path: "creator",
+    select: "account"
   });
 
   if (req.query.skip !== undefined) {
@@ -54,16 +55,16 @@ router.get('/', function(req, res) {
   }
 
   switch (req.query.sort) {
-    case 'price-desc':
+    case "price-desc":
       query.sort({ price: -1 });
       break;
-    case 'price-asc':
+    case "price-asc":
       query.sort({ price: 1 });
       break;
-    case 'date-desc':
+    case "date-desc":
       query.sort({ created: -1 });
       break;
-    case 'date-asc':
+    case "date-asc":
       query.sort({ created: 1 });
       break;
     default:
@@ -74,33 +75,33 @@ router.get('/', function(req, res) {
   });
 });
 
-router.get('/with-counter', function(req, res) {
+router.get("/with-counter", function(req, res) {
   const filter = {};
   if (
-    (req.query.priceMin !== undefined && req.query.priceMin !== '') ||
-    (req.query.priceMax !== undefined && req.query.priceMax !== '')
+    (req.query.priceMin !== undefined && req.query.priceMin !== "") ||
+    (req.query.priceMax !== undefined && req.query.priceMax !== "")
   ) {
     filter.price = {};
     if (req.query.priceMin) {
-      filter.price['$gte'] = req.query.priceMin;
+      filter.price["$gte"] = req.query.priceMin;
     }
 
     if (req.query.priceMax) {
-      filter.price['$lte'] = req.query.priceMax;
+      filter.price["$lte"] = req.query.priceMax;
     }
   }
 
   if (req.query.title) {
     filter.title = {
       $regex: req.query.title,
-      $options: 'i',
+      $options: "i"
     };
   }
 
   Offer.count({}, (err, count) => {
     const query = Offer.find(filter).populate({
-      path: 'creator',
-      select: 'account',
+      path: "creator",
+      select: "account"
     });
 
     if (req.query.skip !== undefined) {
@@ -114,16 +115,16 @@ router.get('/with-counter', function(req, res) {
     }
 
     switch (req.query.sort) {
-      case 'price-desc':
+      case "price-desc":
         query.sort({ price: -1 });
         break;
-      case 'price-asc':
+      case "price-asc":
         query.sort({ price: 1 });
         break;
-      case 'date-desc':
+      case "date-desc":
         query.sort({ created: -1 });
         break;
-      case 'date-asc':
+      case "date-asc":
         query.sort({ created: 1 });
         break;
       default:
@@ -135,33 +136,33 @@ router.get('/with-counter', function(req, res) {
   });
 });
 
-router.get('/with-count', function(req, res) {
+router.get("/with-count", function(req, res) {
   const filter = {};
   if (
-    (req.query.priceMin !== undefined && req.query.priceMin !== '') ||
-    (req.query.priceMax !== undefined && req.query.priceMax !== '')
+    (req.query.priceMin !== undefined && req.query.priceMin !== "") ||
+    (req.query.priceMax !== undefined && req.query.priceMax !== "")
   ) {
     filter.price = {};
     if (req.query.priceMin) {
-      filter.price['$gte'] = req.query.priceMin;
+      filter.price["$gte"] = req.query.priceMin;
     }
 
     if (req.query.priceMax) {
-      filter.price['$lte'] = req.query.priceMax;
+      filter.price["$lte"] = req.query.priceMax;
     }
   }
 
   if (req.query.title) {
     filter.title = {
       $regex: req.query.title,
-      $options: 'i',
+      $options: "i"
     };
   }
 
   Offer.count(filter, (err, count) => {
     const query = Offer.find(filter).populate({
-      path: 'creator',
-      select: 'account',
+      path: "creator",
+      select: "account"
     });
 
     if (req.query.skip !== undefined) {
@@ -175,16 +176,16 @@ router.get('/with-count', function(req, res) {
     }
 
     switch (req.query.sort) {
-      case 'price-desc':
+      case "price-desc":
         query.sort({ price: -1 });
         break;
-      case 'price-asc':
+      case "price-asc":
         query.sort({ price: 1 });
         break;
-      case 'date-desc':
+      case "date-desc":
         query.sort({ created: -1 });
         break;
-      case 'date-asc':
+      case "date-asc":
         query.sort({ created: 1 });
         break;
       default:
@@ -196,19 +197,19 @@ router.get('/with-count', function(req, res) {
   });
 });
 
-router.get('/my-offers', isAuthenticated, function(req, res) {
+router.get("/my-offers", isAuthenticated, function(req, res) {
   Offer.find({ creator: req.user })
-    .populate({ path: 'creator', select: 'account' })
+    .populate({ path: "creator", select: "account" })
     .exec(function(err, offers) {
       res.json(offers);
     });
 });
 
-router.delete('/remove/:id', isAuthenticated, function(req, res, next) {
+router.delete("/remove/:id", isAuthenticated, function(req, res, next) {
   Offer.findOneAndRemove(
     {
       _id: ObjectId(req.params.id),
-      creator: req.user,
+      creator: req.user
     },
     function(err, obj) {
       if (err) {
@@ -216,94 +217,106 @@ router.delete('/remove/:id', isAuthenticated, function(req, res, next) {
       }
       if (!obj) {
         res.status(404);
-        return next('Nothing to delete');
+        return next("Nothing to delete");
       } else {
-        return res.json({ message: 'Deleted' });
+        return res.json({ message: "Deleted" });
       }
     }
   );
 });
 
-router.post('/upload', formidableMiddleware(), isAuthenticated, (req, res) => {
-  console.log('route');
-
-  // les différents clés des fichiers (file1, file2, file3...)
+const uploadPictures = (req, res, next) => {
+  // J'initialise un tableau vide pour y stocker mes images uploadées
+  const pictures = [];
+  // J'initialise le nombre d'upload à zéro
+  let filesUploaded = 0;
+  // Et pour chaque fichier dans le tableau, je crée un upload vers Cloudinary
   const files = Object.keys(req.files);
   if (files.length) {
-    const results = {};
-    // on parcours les fichiers
     files.forEach(fileKey => {
-      // on utilise les path de chaque fichier (la localisation temporaire du fichier sur le serveur)
+      // Je crée un nom spécifique pour le fichier
+      const name = uid2(16);
       cloudinary.v2.uploader.upload(
         req.files[fileKey].path,
         {
-          // on peut préciser un dossier
-          folder: 'some_folder',
+          // J'assigne un dossier spécifique dans Cloudinary pour chaque utilisateur
+          public_id: `leboncoin/${req.user._id}/${name}`
         },
         (error, result) => {
-          // on enregistre le résultat dans un object
+          console.log(error, result);
+          // Si j'ai une erreur avec l'upload, je sors de ma route
           if (error) {
-            results[fileKey] = {
-              success: false,
-              error: error,
-            };
-          } else {
-            results[fileKey] = {
-              success: true,
-              result: result,
-            };
+            return res.status(500).json({ error });
           }
-          if (Object.keys(results).length === files.length) {
-            // tous les uploads sont fait on peut envoyer la réponse
-            return res.json(results);
+          // Sinon, je push mon image dans le tableau
+          pictures.push(result);
+          // Et j'incrémente le nombre d'upload
+          filesUploaded++;
+          console.log("-------\n", result);
+          // Si le nombre d'uploads est égal au nombre de fichiers envoyés...
+          if (filesUploaded === files.length) {
+            /* res
+                        .status(200)
+                        .json({message: `You've uploaded ${filesUploaded} files.`}); */
+            // ... je stocke les images dans l'objet `req`...
+            req.pictures = pictures;
+            // ... et je poursuis ma route avec `next()`
+            next();
           }
         }
       );
     });
   } else {
-    res.send('no file uploaded');
+    // Pas de fichier à uploader ? Je poursuis ma route avec `next()`.
+    next();
   }
-});
+};
 
-router.post('/publish', isAuthenticated, function(req, res, next) {
-  var obj = {
-    title: req.body.title,
-    description: req.body.description,
-    price: req.body.price,
-    pictures: req.pictures,
-    creator: req.user,
-  };
-  var offer = new Offer(obj);
-  offer.save(function(err) {
-    if (!err) {
-      return res.json({
-        _id: offer._id,
-        title: offer.title,
-        description: offer.description,
-        price: offer.price,
-        pictures: offer.pictures,
-        created: offer.created,
-        creator: {
-          account: offer.creator.account,
-          _id: offer.creator._id,
-        },
-      });
-    } else {
-      return next(err.message);
-    }
-  });
-});
+router.post(
+  "/publish",
+  formidableMiddleware(),
+  isAuthenticated,
+  uploadPictures,
+  function(req, res, next) {
+    var obj = {
+      title: req.fields.title,
+      description: req.fields.description,
+      price: req.fields.price,
+      pictures: req.pictures,
+      creator: req.user
+    };
+    var offer = new Offer(obj);
+    offer.save(function(err) {
+      if (!err) {
+        return res.json({
+          _id: offer._id,
+          title: offer.title,
+          description: offer.description,
+          price: offer.price,
+          pictures: offer.pictures,
+          created: offer.created,
+          creator: {
+            account: offer.creator.account,
+            _id: offer.creator._id
+          }
+        });
+      } else {
+        return next(err.message);
+      }
+    });
+  }
+);
 
-router.get('/:id', function(req, res, next) {
+router.get("/:id", function(req, res, next) {
   Offer.findById(req.params.id)
-    .populate({ path: 'creator', select: 'account' })
+    .populate({ path: "creator", select: "account" })
     .exec(function(err, offer) {
       if (err) {
         return next(err.message);
       }
       if (!offer) {
         res.status(404);
-        return next('Not found');
+        return next("Not found");
       } else {
         return res.json(offer);
       }
